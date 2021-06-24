1f6376843364fb606ee2123b2273ecf1c4f16bfb
ruphaa <ruphaaganesh@gmail.com>
2021-06-24 22:59:36 +1000

    Add routes to the menu items

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage.component'
 
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>Hats Page</div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/hats" component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
